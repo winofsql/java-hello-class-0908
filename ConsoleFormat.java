ae34c2d350d400071b096aaad9fe123395467e01
sworc <lightbox@nifty.com>
2021-09-08 15:25:51 +0900

    Add files via upload

--- ConsoleFormat.java
+++ ConsoleFormat.java
@@ -1,18 +1,23 @@
 public class ConsoleFormat {
 
+    private String out;
+
     // コンストラクタ
     public ConsoleFormat() {
-
+        out = "default";
+    }
+    public ConsoleFormat( String out ) {
+        this.out = out;
     }
 
     // メソッド( 外部から利用可能 )
     public void testOutPut( String out ) {
-        System.out.println( String.format("testOutPut : %s", out) );
+        System.out.println( String.format("%s : %s", this.out, out) );
     }
 
     // クラスメソッド( インスタンスを作らなくてもいい )
     public static void testOutPutStatic( String out ) {
-        System.out.println( String.format("testOutPut : %s", out) );
+        System.out.println( String.format("testOutPutStatic : %s", out) );
     }
 
 }
--- HelloClass.java
+++ HelloClass.java
@@ -2,9 +2,14 @@ public class HelloClass {
 
     public static void main (String[] args) {
 
-        ConsoleFormat cf = new ConsoleFormat();
+        ConsoleFormat cf = new ConsoleFormat( "1番目" );
+        ConsoleFormat cf2 = new ConsoleFormat( "ニ番目" );
 
         cf.testOutPut("こんにちは");
+        cf2.testOutPut("こんにちは");
+
+        ConsoleFormat.testOutPutStatic("こんにちは");
+
 
     }
 
